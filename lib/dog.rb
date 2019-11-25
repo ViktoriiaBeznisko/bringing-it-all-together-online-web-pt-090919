a50402d0e4e203c96b05c55499de3d22965a50f1
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-25 03:44:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,7 @@ class Dog
       dog_data = dog[0]
       dog = Dogs.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      dog = self.create(name: name, album: album)
+      dog = self.create(name: name, breed: breed)
     end
     song
   end
