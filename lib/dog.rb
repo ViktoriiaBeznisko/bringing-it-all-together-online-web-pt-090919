516386e1cb4e4280c9d9bbfd1c80b8841ce18291
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-25 03:46:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,16 +68,28 @@ class Dog
     DB[:conn].execute(sql, id).map { |row| new_from_db(row)}.first
   end
   
-  def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if !dog.empty?
-      dog_data = dog[0]
-      dog = Dogs.new(dog_data[0], dog_data[1], dog_data[2])
-    else
-      dog = self.create(name: name, breed: breed)
-    end
-    dog
-  end
+  # def self.find_or_create_by(name:, breed:)
+  #   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+  #   if !dog.empty?
+  #     dog_data = dog[0]
+  #     dog = Dogs.new(dog_data[0], dog_data[1], dog_data[2])
+  #   else
+  #     dog = self.create(name: name, breed: breed)
+  #   end
+  #   dog
+  # end
+  
+  
+      def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+        if !dog.empty?
+          dog_data = dog[0]
+          dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        else
+          dog = self.create(name: name, breed: breed)
+        end
+        dog
+      end
     
   def self.find_by_name(name)
     sql = <<-SQL
